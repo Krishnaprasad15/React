f604dd16cbf68e3591eb5c30a4fa61fc154a1e18
krishna1500 <krishna16151@gmail.com>
2023-07-28 19:06:48 +0530

    Learnig state in class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Greet from './components/greet';
-import Welcome from './components/welcome';
+// import Greet from './components/greet';
+// import Welcome from './components/welcome';
+import Message from "./components/Message"
 function App() {
   return (
     <div className="App">
-      <Greet name="Krishna" nickName="kittu"><p>This is a short intro...</p></Greet>
-      <Greet name="Prasad" nickName="kittu"/>
-      <Welcome name="siri" nickName="siriii"><p>This is a short intro...</p></Welcome>
+      {/* /* <Greet/>
+      <Welcome/> */}
+      <Message/>
+
     </div>
   );
 }
