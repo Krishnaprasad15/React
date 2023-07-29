d62b81772821a8f00f3841f661aafeafdf8d09f9
krishna1500 <krishna16151@gmail.com>
2023-07-29 12:20:48 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React from 'react';
 import './App.css';
 // import Greet from './components/greet';
 // import Welcome from './components/welcome';
-import Message from "./components/Message"
+// import Message from "./components/Message"
+import Counter from "./components/Counter"
 function App() {
   return (
     <div className="App">
       {/* /* <Greet/>
       <Welcome/> */}
-      <Message/>
-
+      {/* <Message/> */}
+      <Counter/>
     </div>
   );
 }
